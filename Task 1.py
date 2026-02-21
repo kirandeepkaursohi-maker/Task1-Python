8d0b938296101d578dd1b00778a6ce51dc814dca
modelmind <kirandeepkaursohi@gmail.com>
2026-02-21 18:22:01 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+a = float(input("Enter the first numer:" ))
+b = float(input("Enter the second numer:" ))
+X = a + b
+Y = a - b
+x = a * b
+y = a / b
+print("Addition", X)
+print("Subtraction", Y)
+print("Multiplication", x)
+print("Division", y)
---
+++ Task 2.py
@@ -0,0 +1,4 @@
+a= input("Your First name: " )
+b= input("Your Second name: " )
+c = a + ' ' + b
+print("Hey, I am", c, "and I glad that I chose tutedue learning.")
